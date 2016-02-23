03582b0e6cead9bce0106a62314a4c1f5a21af96
Russell Simmons <rws10@live.com>
2016-02-23 16:53:29 -0500

    I've removed all of the default page stuff that was generated and added some display attributes to the Idea model.

--- IdeaSite/IdeaSite/Models/Idea.cs
+++ IdeaSite/IdeaSite/Models/Idea.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,27 +11,35 @@ namespace IdeaSite.Models
         public int ideaID { get; set; }
 
         // Name of the idea
+        [Display(Name = "Name")]
         public string name { get; set; }
 
         // What is the idea?
+        [Display(Name = "Description")]
         public string description { get; set; }
 
         // What is the Category of this idea?
+        [Display(Name = "Category")]
         public string category { get; set; }
 
         // What is the date the idea was created?
+        [Display(Name = "Creation Date")]
         public DateTime creationDate { get; set; }
 
         // What is the name of the individual who submitted this idea?
+        //[Display(Name = "Submitter")]
         //public string submitter { get; set; }
 
         // Is the idea Submitted, Active, or Archived?
+        [Display(Name = "Status")]
         public string statusCode { get; set; }
 
         // Why was this idea Denied?
+        [Display(Name = "Reason")]
         public string denialReason { get; set; }
 
         // The attachments on this idea
+        //[Display(Name = "Attachments")]
         //public List<Attachment> attachments { get; set; }
     }
 }
--- IdeaSite/IdeaSite/Startup.cs
+++ IdeaSite/IdeaSite/Startup.cs
@@ -8,7 +8,7 @@ namespace IdeaSite
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            //ConfigureAuth(app);
         }
     }
 }
