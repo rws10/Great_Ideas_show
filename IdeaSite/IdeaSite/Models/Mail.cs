975dce94b705aff104b77ddd4d23ccbbccfd25ad
rws10 <rws10@live.com>
2016-04-15 10:25:49 -0400

    Added the Mail model.

---
+++ IdeaSite/IdeaSite/Models/Mail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdeaSite.Models
+{
+    public class Mail
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
