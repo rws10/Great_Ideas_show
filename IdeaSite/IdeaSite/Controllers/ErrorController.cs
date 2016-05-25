6cb4e008cc0e34210451ee4e65a52aa425ab406f
AWGarvin <alexwgarvin@gmail.com>
2016-05-25 09:33:03 -0700

    A few improvements on the logging. May be overwriting and not appending. For some reason after one of my pulls the incorrect views are being desplayed for the errors, though the URL shows the right code is being shown.

--- IdeaSite/IdeaSite/Controllers/ErrorController.cs
+++ IdeaSite/IdeaSite/Controllers/ErrorController.cs
@@ -11,34 +11,20 @@ namespace IdeaSite.Controllers
     public class ErrorController : Controller
     {
 
-        String filename = AppDomain.CurrentDomain.BaseDirectory + "App_Data\\" + "log\\" + "logErrors.txt";
-
         public ViewResult Index()
         {
-            //var sw = new System.IO.StreamWriter(filename, true);
             using (StreamWriter tw = new StreamWriter(@"C:\Users\alex\Desktop\log.txt"))
             {
                 tw.Write("UNSPECIFIED ERROR");
             }
-            /*
-            sw.WriteLine(DateTime.Now.ToString() + " UNKNOWN ERROR");
-            sw.Close();
-            System.IO.File.WriteAllText(@"C:\Users\alex\Desktop\log.txt", "text");
-            */
             return View("Error");
         }
         
         public ViewResult Forbidden()
         {
-            /*
-            var sw = new System.IO.StreamWriter(filename, true);
-            sw.WriteLine(DateTime.Now.ToString() + " STATUSCODE 403");
-            sw.Close();
-            System.IO.File.WriteAllText(@"C:\Users\alex\Desktop\log.txt", "text");
-            */
             using (StreamWriter tw = new StreamWriter(@"C:\Users\alex\Desktop\log.txt"))
             {
-                tw.Write("ERROR CODE 403: FORBIDDEN");
+                tw.WriteLine("ERROR CODE 403: FORBIDDEN");
             }
             Response.StatusCode = 403;
             return View("Forbidden");
@@ -46,52 +32,32 @@ namespace IdeaSite.Controllers
 
         public ViewResult NotFound()
         {
-            /*
-            var sw = new System.IO.StreamWriter(filename, true);
-            sw.WriteLine(DateTime.Now.ToString() + " STATUSCODE 404");
-            sw.Close();
-            System.IO.File.WriteAllText(@"C:\Users\alex\Desktop\log.txt", "text");
-            */
+            Response.StatusCode = 404;
             using (StreamWriter tw = new StreamWriter(@"C:\Users\alex\Desktop\log.txt"))
             {
-                tw.Write("ERROR CODE 404: NOT FOUND");
+                tw.WriteLine("ERROR CODE 404: NOT FOUND");
             }
-            Response.StatusCode = 404;
-            return View("Error/NotFound");
+            return View("NotFound");
         }
 
         public ViewResult InternalServerError()
         {
-            /*
-            var sw = new System.IO.StreamWriter(filename, true);
-            sw.WriteLine(DateTime.Now.ToString() + " STATUSCODE 500");
-            sw.Close();
-            System.IO.File.WriteAllText(@"C:\Users\alex\Desktop\log.txt", "text");
-            */
+            Response.StatusCode = 500;
             using (StreamWriter tw = new StreamWriter(@"C:\Users\alex\Desktop\log.txt"))
             {
-                tw.Write("ERROR CODE 500: INTERNAL SERVER ERROR");
+                tw.WriteLine("ERROR CODE 500: INTERNAL SERVER ERROR");
             }
-            Response.StatusCode = 500;
             return View("InternalServerError");
         }
 
         public ViewResult Unauthorized()
         {
-            /*
-            var sw = new System.IO.StreamWriter(filename, true);
-            sw.WriteLine(DateTime.Now.ToString() + " STATUSCODE 401");
-            sw.Close();
-            System.IO.File.WriteAllText(@"C:\Users\alex\Desktop\log.txt", "text");
-            */
+            Response.StatusCode = 401;
             using (StreamWriter tw = new StreamWriter(@"C:\Users\alex\Desktop\log.txt"))
             {
-                tw.Write("ERROR CODE 401: UNAUTHORIZED");
+                tw.WriteLine("ERROR CODE 401: UNAUTHORIZED");
             }
-            Response.StatusCode = 401;
             return View("Unauthorized");
         }
-
-
     }
 }
