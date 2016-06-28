925079470c14c58a5f1a8916598329ed2f006099
Russell Simmons <rws10@live.com>
2016-06-28 09:01:07 -0400

    pushing a file I missed.

--- IdeaSite/IdeaSite/Models/Idea.cs
+++ IdeaSite/IdeaSite/Models/Idea.cs
@@ -29,6 +29,7 @@ namespace IdeaSite.Models
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime cre_date { get; set; }
 
+
         // What is the date of the last modification?
         [Display(Name = "Modification Date")]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
@@ -38,18 +39,24 @@ namespace IdeaSite.Models
         [Display(Name = "Name")]
         public string cre_user { get; set; }
 
+        // The email of the submitter
+        [Display(Name = "Submitter Email")]
+        public string email { get; set; } 
+
         // Is the idea Submitted, Accepted, Rejected, Archived, or submitted as a project?
-        // Admin needs to be able to make this "Project Submission" as well. Possibly disable edit after this?
         [Required]
         [Display(Name = "Status")]
         public string statusCode { get; set; }
 
         // Why was this idea Denied?
-        // This needs to be a conditional display of only when the status is set to "Rejected"
         [DataType(DataType.MultilineText)]
         [Display(Name = "Reason for Decision")]
         public string denialReason { get; set; }
 
-        public virtual List<Attachment> attachments { get; set; }
+        // Number of comments on the idea
+        [Display(Name = "Number of Comments")]
+        public int commentsNumber { get; set; }
+
+        public List<Attachment> attachments { get; set; }
     }
 }
