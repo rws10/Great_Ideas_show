daa6de379c0cb131127620d998357185aadb493c
Russell Simmons <rws10@live.com>
2016-09-12 11:35:31 -0400

    Various changes to add log4net and a few others. Also gets rid of IdeaSiteContext and Migrations

--- IdeaSite/IdeaSite/Models/Attachment.cs
+++ IdeaSite/IdeaSite/Models/Attachment.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections;
 using System.IO;
-using log4net;
 
 namespace IdeaSite.Models
 {
